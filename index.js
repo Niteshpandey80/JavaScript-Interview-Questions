20bf04abba46ef3228444f084714a95e5f6a707e
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2026-01-24 23:53:29 +0530

    Q13

--- index.js
+++ index.js
@@ -147,3 +147,17 @@ for(var i=0 ; i<arr.length ; i++){
 console.log(sl);
 
 Q 14 Move all zeroes to the end of the array. 
+
+const arr = [0, 1, 0, 3, 12];
+let index = 0 ;
+for(var i=0 ; i<arr.length ; i++){
+   if(arr[i]!== 0 ){
+    arr[index] = arr[i];
+    index++;
+   }
+}
+for(let i=index ; i<arr.length ; i++){
+    arr[i]=0;
+}
+console.log(arr);
+
